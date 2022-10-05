7ce94774e82a0533ae7056aef3b8ebf8acf14a3c
jbijlsma <jeroen_bijlsma@yahoo.com>
2022-10-05 11:11:35 +0800

    Add example for aws sam (lambda)

---
+++ aws-sam-api-ts/api/index.ts
@@ -0,0 +1,19 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+import middy from "@middy/core";
+
+export const lambdaHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const msg = event.pathParameters?.message;
+  const response = { message: "Hello World, says hello " + msg };
+
+  const result: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: JSON.stringify(response),
+  };
+
+  return result;
+};
+
+export const handler = middy(lambdaHandler);
